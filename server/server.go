69272114de03e6582b43bbdc9290e0f702830cef
Jason Tevnan <jason.tevnan@gmail.com>
2023-06-05 12:36:00 +0200

    increase logging

--- server/server.go
+++ server/server.go
@@ -31,12 +31,13 @@ func (f FuzzyHTTPServer) Status(w http.ResponseWriter, r *http.Request) {
 }
 
 func (f FuzzyHTTPServer) Api(w http.ResponseWriter, r *http.Request) {
-	logrus.Debugf("api call %d", f.Settings.Timeout)
+	logrus.Debugf("api call %d start", f.Settings.Timeout)
 	n := rand.Intn(f.Settings.Timeout * 1000) // n will be between 0 and 10
 	time.Sleep(time.Duration(n) * time.Millisecond)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, `{"status": "ok"}`)
 	f.Metrics.ReuestCount.Inc()
+	logrus.Debugf("api call %d end", f.Settings.Timeout)
 	return
 }
